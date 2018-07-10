345b26d08489b42b22263d82034313024082c434
Ernestas <ernestas.klibavicius@gmail.com>
2018-07-10 11:18:56 +0300

    init commit

---
+++ client/src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url('../assets/styles/style.css');
+</style>
